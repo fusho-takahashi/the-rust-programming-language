db4b0a4a4e9778da857e79165a9a2b71a8715e9c
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-09-11 15:46:15 +0900

    cargo によるbuild

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/fusho-takahashi/work/rust/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/fusho-takahashi/work/rust/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/fusho-takahashi/work/rust/hello_cargo/target/debug/hello_cargo: /Users/fusho-takahashi/work/rust/hello_cargo/src/main.rs
